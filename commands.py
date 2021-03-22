80bff58e5c2dbf57aba4a0577e4f12f983af1ca2
Xanthus84 <Vladimirthebest@yandex.ru>
2021-03-22 16:59:26 +0300

    Реализован функционал работы со списками среднесрочных и перспективных дел

--- commands.py
+++ commands.py
@@ -7,8 +7,8 @@ db = DatabaseManager('bookmarks.db')  # <1> открывает БД, если т
 
 
 class CreateBookmarksTableCommand: # команда для создания таблицы в БД
-    def execute(self):  # <2> команда для создания таблицы с определенными заголовками при запуске приложения
-        db.create_table('bookmarks', {  # <3> создает таблица используя функцию create_table
+    def execute(self, name):  # <2> команда для создания таблицы с определенными заголовками при запуске приложения
+        db.create_table(name, {  # <3> создает таблица используя функцию create_table
             'id': 'integer primary key autoincrement', # id каждой записи автоматически наращивается по мере добавления записей
             'title': 'text not null', # not null требует, чтобы столбец был заполнен значениями
             # 'url': 'text not null',
@@ -17,9 +17,9 @@ class CreateBookmarksTableCommand: # команда для создания та
         })
 
 class AddBookmarkCommand: # команда добавления закладки
-    def execute(self, data):
+    def execute(self, name, data):
         data['date_added'] = datetime.today().strftime('%d.%m.%Y')  #.isoformat()  # <1> добавляет текущую дату и время при добавлении записи
-        db.add('bookmarks', data)  # <2>
+        db.add(name, data)  # <2>
         return 'Заметка загружена в БД!'  # <3>
 
 
@@ -27,18 +27,18 @@ class ListBookmarksCommand: # команда для вызова на экран
     def __init__(self, order_by='date_added'):  # <1>
         self.order_by = order_by
 
-    def execute(self):
-        return db.select('bookmarks', order_by=self.order_by).fetchall()  # <2>
+    def execute(self, name):
+        return db.select(name, order_by=self.order_by).fetchall()  # <2>
 
 
 class DeleteBookmarkCommand: # команда для удаления закладок
-    def execute(self, data):
-        db.delete('bookmarks', {'id': data})  # <1> delete принимает словарь имен столбцов и сопоставляет пары значений
+    def execute(self, name, data):
+        db.delete(name, {'id': data})  # <1> delete принимает словарь имен столбцов и сопоставляет пары значений
         return 'Заметка удалена!'
 
 class UpdateBookmarkCommand: # команда для удаления закладок
-    def execute(self, data, note):
-        db.update('bookmarks', {'id': data}, note)  # <1> delete принимает словарь имен столбцов и сопоставляет пары значений
+    def execute(self, name, data, note):
+        db.update(name, {'id': data}, note)  # <1> delete принимает словарь имен столбцов и сопоставляет пары значений
         return 'Заметка изменена!'
 
 # class QuitCommand:
